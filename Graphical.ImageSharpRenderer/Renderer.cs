d2d785c25cdddcc809c306234aed2f6856a8f122
Linus <linus.schneeberg@gmail.com>
2026-04-11 20:20:24 +0200

    blabla

--- Graphical.ImageSharpRenderer/Renderer.cs
+++ Graphical.ImageSharpRenderer/Renderer.cs
@@ -16,27 +16,67 @@ public static class Renderer
 
         foreach (Primitive p in g.Primitives)
         {
-            using Image<Rgba32> primtiveImg = renderPrimitive(p);
-            primtiveImg.Mutate(ctx =>
+            using Image<Rgba32>? primitiveImg = renderPrimitive(p);
+            if (primitiveImg is null)
+                continue;
+            primitiveImg.Mutate(ctx =>
                 ctx.Transform(new AffineTransformBuilder().AppendMatrix(p.Transform.ToMatrix()))
             );
-            img.Mutate(ctx => ctx.DrawImage(primtiveImg, 1f));
+            img.Mutate(ctx => ctx.DrawImage(primitiveImg, 1f));
         }
 
         return img;
     }
 
-    private static Image<Rgba32> renderPrimitive(
+    private static Image<Rgba32> getCanvas(
+        float left,
+        float top,
+        float right,
+        float bottom,
+        Transform t,
+        out Vector2 offset
+    )
+    {
+        int width = (int)(right - left);
+        int height = (int)(bottom - top);
+        offset = new(-left, -right);
+
+        return new(width, height);
+    }
+
+    private static Image<Rgba32>? renderPrimitive(
         Primitive p,
         bool ignoreMissingImplementation = true
     )
     {
+        Image<Rgba32> canvas;
         switch (p)
         {
+            case Composite c:
+                IEnumerable<(Primitive p, Image<Rgba32> img)> rendered = c.GetPrimitives()
+                    .Select(p => (p, renderPrimitive(p)))
+                    .OfType<(Primitive, Image<Rgba32>)>();
+
+                canvas = new(rendered.Max(r => r.p.), rendered.Max(r => r.Bounds.Height));
+                float left = rendered.Min(r => r.Bounds.)
+                canvas.Mutate(ctx =>
+                {
+                    foreach (Image<Rgba32> render in rendered)
+                    {
+                        ctx.DrawImage(render, new Point(0, 0), 1f);
+                    }
+                });
+                return canvas;
+
             case Rectangle r:
+                if ((int)r.Width == 0 || (int)r.Height == 0)
+                {
+                    return null;
+                }
                 return new Image<Rgba32>((int)r.Width, (int)r.Height, r.Paint.Fill.ToRgba32());
+
             case Triangle t:
-                Image<Rgba32> canvas = new(
+                canvas = new(
                     (int)((IEnumerable<Vector2>)[t.A, t.B, t.C]).Max(v => v.X),
                     (int)((IEnumerable<Vector2>)[t.A, t.B, t.C]).Max(v => v.Y)
                 );
@@ -49,6 +89,9 @@ public static class Renderer
                     )
                 );
                 return canvas;
+
+            case Text t:
+                throw new NotImplementedException();
             default:
                 return ignoreMissingImplementation
                     ? new Image<Rgba32>(1, 1)
--- Graphical.Tests/Program.cs
+++ Graphical.Tests/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Numerics;
+using Datastructures;
 using Graphical;
 using Graphical.ImageSharpRenderer;
 using Graphical.Primitives;
@@ -19,6 +20,17 @@ Graphic g = new Graphic()
         new(300, 0),
         transform: new Transform(new(0, 0), (float)(Math.PI / 5), new Vector2(10)),
         paint: new Paint(Color.Blue, Color.Transparent)
+    )
+    .With(
+        new CircleInTriangle(
+            200,
+            Transform: new Transform(new(400, 400), 0f, Vector2.One),
+            Paint: new Paint(Color.Green, Color.Transparent)
+        )
     );
 
 Renderer.Render(g, 1920, 1080).SaveAsPng("out.png");
+
+VisualisedArray<int> a = new([1, 2, 3, 4]);
+
+Renderer.Render(a.Render(), 1920, 1080).SaveAsPng("array.png");
